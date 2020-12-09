1ef5d132367ca77c252906c80143273707686867
Chaltu Abdulkadir <chaltuabdulkadir@Chaltus-MacBook.local>
2020-12-08 22:19:39 -0600

    fix typo

--- javaScript.js
+++ javaScript.js
@@ -1,33 +1,35 @@
 var box = document.getElementById("box")
 var growBtn = document.getElementById("grow")
 var blueBtn = document.getElementById("blue")
-var fadeBtn = document.getElementById("Fade")
+var fadeBtn = document.getElementById("fade")
 var resetBtn = document.getElementById("reset")
 
-document.getElementById("growBtn").addEventListener("click", function(){
 
-    document.getElementById("box").style.height = "300px";
-    document.getElementById("box").style.width = "300px";
+growBtn.addEventListener("click", function(){
+
+    box.style.height = "300px";
+    box.style.width = "300px";
 });
 
-document.getElementById("blueBtn").addEventListener("click", function(){
+blueBtn.addEventListener("click", function(){
 
-    document.getElementById("box").style.backgroundColor = "blue";
+    box.style.backgroundColor = "blue";
 
 });
 
-document.getElementById("fadeBtn").addEventListener("click",function(){
-    document.getElementById("box").style.opacity = 0.1;
-    ;
+fadeBtn.addEventListener("click",function(){
+    box.style.opacity = 0.1;
+    
 
-})
+});
 
-document.getElementById("resetBtn").addEventListener("click", function(){
+resetBtn.addEventListener("click", function(){
+console.log("button")
+    box.style.height = "150px";
+    box.style.width = "150px";
+    box.style.backgroundColor = "orange";
+    box.style.opacity = 50;
 
-    document.getElementById("box").style.height = "150px";
-    document.getElementById("box").style.width = "150px";
-    document.getElementById("box").style.backgroundColor = "orange";
-    document.getElementById("box").style.opacity = 50;
 });
 
 
